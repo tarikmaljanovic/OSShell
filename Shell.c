03eee2565478b9251e1c30b3a67acbdcd19cdc10
Tarik Maljanovic <tarikmaljanovic123@gmail.com>
2023-04-04 19:42:55 +0200

    Shell redesign

--- Shell.c
+++ Shell.c
@@ -28,56 +28,32 @@
 //Recursice promp function
 void prompt() {
   char input[100]; //User input is stored here
+  char *arguments[100];
   char hostName[100]; //Host name is stored here
   char *login = getlogin(); //Login name is stored and called here
-
   gethostname(hostName, 100); //Function for getting the host name of the machine
   
 
-  printf(RED"%s@%s\033[0m\033[1;32m:~$\033[0m ", hostName, login);  //The prompt which looks like this "machineName@userName:~$""
-  scanf("%s", input); //Get input from user
+  printf(RED"%s@%s\033[0m"BOLDGREEN":~$\033[0m ", hostName, login);  //The prompt which looks like this "machineName@userName:~$""
+  fgets(input, 100, stdin);
 
-  if(strcmp(input, "quit") == 0) { //If user enteres "quit", the shell prints a goodbye message and stops the prompt() funnction
-    printf(BOLDBLUE"Goodbye ( ´ ▽ ` )ﾉ\033[0m\n");
-    return;
-  //The following code is for the cases when the use enteres "wc", "grep", "df", "cmatrix"
-  //The way these commands are executes, is when the user enteres the command name, a child process is created and the execl() function
-  //is used to transofrm the chils process into the implementations of these commands (which are the executables included wothin this .zip folder)
-  //The parent process (the prompt) will wait for the child process and then resume once done
-  } else if (strcmp(input, "wc") == 0) {
-    int rc = fork();
-    if(rc == 0) {
-      execl("wc", NULL);
-    } else if (rc > 0) {
-      wait(NULL);
+  input[strlen(input) - 1] = '\0';
+  char *token = strtok(input, " ");
+    int i = 0;
+    while (token != NULL && i < 100) {
+      arguments[i] = token;
+      token = strtok(NULL, " ");
+      i++;
     }
-  } else if(strcmp(input, "grep") == 0) {
-    int rc = fork();
-    if(rc == 0) {
-      execl("grep", NULL);
-    } else if (rc > 0) {
-      wait(NULL);
-    }
-  } else if (strcmp(input, "name") == 0) { //Print the shell name
-    printf("Shell Name: "BOLDBLUE"Shelly v1.0.0\033[0m\n");
-  } else if (strcmp(input, "df") == 0) {
-    int rc = fork();
-    if(rc == 0) {
-      execl("df", NULL);
-    } else if (rc > 0) {
-      wait(NULL);
-    }
-  } else if(strcmp(input, "cmatrix") == 0) {
-    int rc = fork();
-    if(rc == 0) {
-      execl("cmatrix", NULL);
-    } else if (rc > 0) {
-      wait(NULL);
-    }
-  } else if(strcmp(input, "clear") == 0) { //Use the "clear" shell command for clearing the screen when it gets too messy
-    system("clear"); //Not actually implemented, just used the system() function which executes shell commands (of the actual shell of the machine)
-  } else if(strcmp(input, "man") == 0) {
-    //A simple man page of this shell, which explaines each available command
+    arguments[i] = NULL;
+
+    if(strcmp(arguments[0], "quit") == 0) {
+      printf(BOLDBLUE"Goodbye ( ´ ▽ ` )ﾉ\033[0m\n");
+      return;
+    } else if(strcmp(arguments[0], "name") == 0) {
+      printf("Shell Name: "BOLDBLUE"Shelly v1.0.0\033[0m\n");
+    } else if(strcmp(arguments[0], "manual") == 0) {
+      //A simple man page of this shell, which explaines each available command
     printf(BOLDBLUE"Shell Man Page\033[0m\n");
     printf("\n");
     printf("Shell Name: "BOLDBLUE"Shelly v1.0.0\033[0m\n");
@@ -111,9 +87,18 @@ void prompt() {
 
     printf(BOLDBLUE"man ~Get manual page of the shell\033[0m\n");
     printf(BOLDGREEN"**************************\033[0m\n");
+    } else {
+      int rc = fork();
 
-  }
-
+      if(rc == 0) {
+        execvp(arguments[0], arguments);
+      } else if(rc > 0) {
+        wait(NULL);
+      } else if (rc < 0) {
+        perror("Failed to fork\n");
+        exit(1);
+      }
+    }
   prompt();
 }
 
